4cdad16574740809165e7319aa08110603400ff6
Jesse <jchisu31@gmail.com>
2015-01-28 09:19:04 -0800

    Blog- First Update

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            echo "Website is runnning locally, also this is a website.";
+        ?>
+    </body>
+</html>
