893988092cac20474ade136e601f2f8952db47ea
Jesse <jchisu31@gmail.com>
2015-01-29 09:21:10 -0800

    Blog- Update

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     </head>
     <body>
         <?php
-            echo "Website is runnning locally, also this is a website.";
+            require_once(__DIR__ . "/controller/create-db.php");
         ?>
     </body>
 </html>
