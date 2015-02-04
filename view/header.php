ef28a274b9353ebc89344beed9b2bafb92f6c83f
Jesse <jchisu31@gmail.com>
2015-02-04 08:50:20 -0800

    Blog-Update 3?

--- view/header.php
+++ view/header.php
@@ -1,5 +1,7 @@
 <html>
-    <head>
+    <head>        
+        <link type="text/css" rel="stylesheet" href="css/bootstrap-theme.css" >
+        <link type="text/css" rel="stylesheet" href="css/bootstrap.css">
         <meta charset="UTF-8">
         <title></title>
     </head>
